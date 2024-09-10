d1219c34c3252903aa7e0764669457f3661ea1d2
Chris Bowen <4783294+cbbowen@users.noreply.github.com>
2024-09-09 23:21:48 -0700

    Use new leptos shell

--- src/lib.rs
+++ src/lib.rs
@@ -17,29 +17,37 @@ pub mod test;
 
 use leptos::prelude::*;
 use leptos_meta::*;
-use leptos_router::*;
 use leptos_router::components::*;
+use leptos_router::*;
 
 #[component]
 pub fn App() -> impl IntoView {
 	provide_meta_context();
 
+	let configuration = leptos::prelude::get_configuration(None).unwrap();
+	let options = configuration.leptos_options;
+
 	view! {
-		<Html/>
-
-		<Title formatter=|page| format!("Stark - {page}")/>
-
-		// Inject metadata in the <head> tag.
-		<Meta charset="UTF-8"/>
-		<Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-		<pages::Home/>
-		// <Router>
-		// 	<Routes>
-		// 		// TODO: Figure out best to handle routes. When deployed on Github pages, this will be under /stark, but when testing locally with trunk, it won't.
-		// 		<Route path="/stark" view=pages::Home/>
-		// 		<Route path="/*" view=|| view! { <Redirect path="/stark"/> }/>
-		// 	</Routes>
-		// </Router>
+		<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<meta charset="utf-8"/>
+				<meta name="viewport" content="width=device-width, initial-scale=1"/>
+				<Title formatter=|page| format!("Stark - {page}")/>
+				<AutoReload options=options.clone()/>
+				<HydrationScripts options/>
+				<MetaTags/>
+			</head>
+			<body>
+				<pages::Home />
+				// <Router>
+				// <Routes>
+				// // TODO: Figure out best to handle routes. When deployed on Github pages, this will be under /stark, but when testing locally with trunk, it won't.
+				// <Route path="/stark" view=pages::Home/>
+				// <Route path="/*" view=|| view! { <Redirect path="/stark"/> }/>
+				// </Routes>
+				// </Router>
+			</body>
+		</html>
 	}
 }
