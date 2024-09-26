0a17a88b597faf93aa6e6bae150d55d9fb79d6c9
Chris Bowen <4783294+cbbowen@users.noreply.github.com>
2024-09-26 00:15:40 -0700

    Cache embedded shape

--- src/engine/embedded_shapes.rs
+++ src/engine/embedded_shapes.rs
@@ -1,3 +1,5 @@
+use std::sync::OnceLock;
+
 // Long term, we probably don't want to embed the large assets.
 
 use crate::util::*;
@@ -12,9 +14,19 @@ pub struct Shape {
 	pub values: Vec<f32>,
 }
 
+pub fn get_image_00507() -> &'static Image {
+	static IMAGE: OnceLock<Image> = OnceLock::new();
+	IMAGE.get_or_init(|| {
+		let mut image = Image::read(RAW_00507_PNG, Default::default()).unwrap();
+		image.convert_color(ColorSpace::Luma).unwrap();
+		image.convert_to_f32_subpixels();
+		image
+	})
+}
+
+// TODO: Remove this.
 pub fn get_shape_00507() -> Shape {
-	let mut image = Image::read(RAW_00507_PNG, Default::default()).unwrap();
-	image.convert_color(ColorSpace::Luma).unwrap();
+	let mut image = get_image_00507().clone();
 	let (width, height) = image.dimensions();
 
 	Shape {
